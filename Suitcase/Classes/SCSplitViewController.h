4464b8fe70997f557d2cfc95073b40ce4117e252
Sebastian Staudt <koraktor@gmail.com>
2013-11-07 18:55:16 +0100

    Implement a custom split view controller
    This allows for setting the status bar style on iOS 7.

---
+++ Suitcase/Classes/SCSplitViewController.h
@@ -0,0 +1,12 @@
+//
+//  SCSteamIdFormController.m
+//  Suitcase
+//
+//  Copyright (c) 2013, Sebastian Staudt
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SCSplitViewController : UISplitViewController
+
+@end
---
+++ Suitcase/Classes/SCSplitViewController.m
@@ -0,0 +1,17 @@
+//
+//  SCSteamIdFormController.m
+//  Suitcase
+//
+//  Copyright (c) 2013, Sebastian Staudt
+//
+
+#import "SCSplitViewController.h"
+
+@implementation SCSplitViewController
+
+- (UIStatusBarStyle)preferredStatusBarStyle
+{
+    return UIStatusBarStyleLightContent;
+}
+
+@end
