fce07bda9424ee05c7e6ae637d924010396f00b2
Sebastian Staudt <koraktor@gmail.com>
2014-09-23 07:50:49 +0200

    Improve colors in multi-value settings

---
+++ Suitcase/Classes/IASKSpecifierValuesViewController+Suitcase.m
@@ -0,0 +1,17 @@
+//
+//  IASKSpecifierValuesViewController+Suitcase.m
+//  Suitcase
+//
+//  Copyright (c) 2014, Sebastian Staudt
+//
+
+#import "IASKSpecifierValuesViewController+Suitcase.h"
+
+@implementation IASKSpecifierValuesViewController (Suitcase)
+
+- (void)tableView:(UITableView *)tableView willDisplayCell:(UITableViewCell *)cell forRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    cell.backgroundColor = [UIColor whiteColor];
+}
+
+@end
