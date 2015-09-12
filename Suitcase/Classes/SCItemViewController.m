c144ccaef3d0ae9b09242c046799f0060a338062
Sebastian Staudt <koraktor@gmail.com>
2015-09-12 14:48:25 +0200

    Adapt to App Transport Security

--- Suitcase/Classes/SCItemViewController.m
+++ Suitcase/Classes/SCItemViewController.m
@@ -236,7 +236,7 @@ typedef enum {
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
 {
     if ([[segue identifier] isEqualToString:@"showWikiPage"]) {
-        NSURL *wikiUrl = [NSURL URLWithString:[NSString stringWithFormat:@"http://wiki.teamfortress.com/scripts/itemredirect.php?id=%@&lang=%@", ((SCWebApiItem *)self.item).defindex, [[SCLanguage currentLanguage] localeIdentifier]]];
+        NSURL *wikiUrl = [NSURL URLWithString:[NSString stringWithFormat:@"https://wiki.teamfortress.com/scripts/itemredirect.php?id=%@&lang=%@", ((SCWebApiItem *)self.item).defindex, [[SCLanguage currentLanguage] localeIdentifier]]];
 
         UIWebView *webView;
         if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPad) {
